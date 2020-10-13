a4bfd7df323e9cbf521379e5c10623ca8f846299
Petru Marian Burlacu <40285539@live.napier.ac.uk>
2020-10-13 18:40:18 +0300

    Add jest setup, sonar settings

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AppComponent, data: {title: 'Enterpulse', breadcrumb: 'Home'} },
+  { path: '**', component: AppComponent, data: {title: 'Enterpulse', breadcrumb: 'Wondering'} },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
